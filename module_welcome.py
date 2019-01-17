d586cb990c3614f332e90be016a8055ece4d5478
Crysalix <yannick013@gmail.com>
2019-01-17 10:30:01 +0100

    check ifenabled

--- module_welcome.py
+++ module_welcome.py
@@ -16,20 +16,18 @@ class Welcome:
 
     async def on_member_join(self, member):
         serverlistmodules = readData('server', member.server.id)
-        if serverlistmodules["welcome"]["last"] == "disabled":
-            return
-        fmt = serverlistmodules['welcome']['config']['message']['value']
-        channel = str(''.join(filter(str.isdigit, serverlistmodules['welcome']['config']['channel']['value'])))
-        member.id = '<@' + member.id + '>'
-        await self.bot.send_message(self.bot.get_channel(channel), fmt.format(user=member.id))
+        if serverlistmodules["welcome"]["last"] == "enabled":
+            fmt = serverlistmodules['welcome']['config']['message']['value']
+            channel = str(''.join(filter(str.isdigit, serverlistmodules['welcome']['config']['channel']['value'])))
+            member.id = '<@' + member.id + '>'
+            await self.bot.send_message(self.bot.get_channel(channel), fmt.format(user=member.id))
 
     async def on_member_remove(self, member):
         serverlistmodules = readData('server', member.server.id)
-        if serverlistmodules["welcome"]["last"] == "disabled":
-            return
-        fmt = '{user} nous a quitté. :wave:'
-        channel = str(''.join(filter(str.isdigit, serverlistmodules['welcome']['config']['channel']['value'])))
-        await self.bot.send_message(self.bot.get_channel(channel), fmt.format(user=member.name))
+        if serverlistmodules["welcome"]["last"] == "enabled":
+            fmt = '{user} nous a quitté. :wave:'
+            channel = str(''.join(filter(str.isdigit, serverlistmodules['welcome']['config']['channel']['value'])))
+            await self.bot.send_message(self.bot.get_channel(channel), fmt.format(user=member.name))
 
 def setup(bot):
     bot.add_cog(Welcome(bot))
